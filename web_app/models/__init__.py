92512265603e47b5b42d2e4e659facac76221965
Tatyanka98 <gladkih.tatyana98@gmail.com>
2025-09-28 15:02:18 +0500

    fix(db): цикличный форейгн_кей

--- web_app/models/__init__.py
+++ web_app/models/__init__.py
@@ -12,29 +12,40 @@ from .user_achievement import UserAchievements
 from .user_task import UserTasks
 from .registration_link import RegistrationLinks
 
-Users.group = ForeignKeyField(
-    Groups,
-    backref='students',
-    column_name='group_id',
-    null=True
-)
-
-Groups.teacher = ForeignKeyField(
-    Users,
-    backref='teaches_groups',
-    column_name='teacher_id',
-    null=True
-)
-
-Users.project = ForeignKeyField(
-    Projects,
-    backref='participants',
-    column_name='project_id',
-    null=True
-)
 
 tables = [Groups, Users, Projects, Achievements, RegistrationLinks, Tasks, UserAchievements, UserTasks]
 db.create_tables(tables, safe=True)
+
+db.execute_sql('ALTER TABLE users DROP COLUMN IF EXISTS group_id CASCADE')
+db.execute_sql('ALTER TABLE users DROP COLUMN IF EXISTS project_id CASCADE')
+db.execute_sql('ALTER TABLE groups DROP COLUMN IF EXISTS teacher_id CASCADE')
+db.execute_sql('''
+        ALTER TABLE users 
+        ADD COLUMN group_id INTEGER REFERENCES groups(id) ON DELETE SET NULL
+    ''')
+
+db.execute_sql('''
+        ALTER TABLE users
+        ADD COLUMN project_id INTEGER REFERENCES projects(id) ON DELETE SET NULL
+    ''')
+
+db.execute_sql('''
+        ALTER TABLE groups
+        ADD COLUMN teacher_id INTEGER REFERENCES users(id) ON DELETE SET NULL
+    ''')
+
+Users_orig = Users
+Groups_orig = Groups
+
+
+class Users(Users_orig):
+    group = ForeignKeyField(Groups, column_name='group_id', backref='students', null=True)
+    project = ForeignKeyField(Projects, column_name='project_id', backref='participants', null=True)
+
+
+class Groups(Groups_orig):
+    teacher = ForeignKeyField(Users, column_name='teacher_id', backref='teaches_groups', null=True)
+
 
 __all__ = [
     "BaseModel",
